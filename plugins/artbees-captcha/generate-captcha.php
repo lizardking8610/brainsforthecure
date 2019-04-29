2343d9045cceb62a0b1dba16c1ada95943c9da24
Hayley Anthony <hayley.d.anthony@gmail.com>
2019-04-29 05:36:15 -0400

    Remove Unused Plugins

--- plugins/artbees-captcha/generate-captcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(dirname( __FILE__ ) . '/captcha.php');
-
-session_start();
-$captcha = new Mk_Artbees_Captcha();
-
-$captcha->imageFormat = 'png';
-$captcha->CreateImage();
--- plugins/surbma-yoast-breadcrumb-shortcode/README.md
+++
@@ -1,4 +0,0 @@
-Surbma - Yoast SEO Breadcrumb Shortcode
-=======================================
-
-A simple shortcode to include Yoast SEO's breadrumb function into WordPress.
